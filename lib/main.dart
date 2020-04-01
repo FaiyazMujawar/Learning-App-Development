8cbaa7edee2cbe50dbd7c75738ec3725d1e298f9
Faiyaz <mujawar.faiyaz.aziz@gmail.com>
2020-04-01 22:50:59 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,93 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.blueGrey[900],
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/faiyaz.jpg'),
+                ),
+                Text(
+                  'Faiyaz Mujawar',
+                  style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.white,
+                      letterSpacing: 1.25,
+                      fontFamily: 'Grand Hotel'),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'DEVELOPER',
+                  style: TextStyle(
+                    fontSize: 13,
+                    color: Colors.white,
+                    fontFamily: 'Source Sans Pro',
+                    letterSpacing: 2,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                  width: 80,
+                  child: Divider(
+                    color: Colors.white,
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      size: 20,
+                      color: Colors.blueGrey[900],
+                    ),
+                    title: Text(
+                      '+91 8796820910',
+                      style: TextStyle(
+                        color: Colors.blueGrey[900],
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      size: 20,
+                      color: Colors.blueGrey[900],
+                    ),
+                    title: Text(
+                      'mujawar.faiyaz.aziz@gmail.com',
+                      style: TextStyle(
+                        color: Colors.blueGrey[900],
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
