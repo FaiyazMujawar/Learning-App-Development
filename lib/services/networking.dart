5909ed60ea0b61323a31042371ff34831fcc129b
Faiyaz <mujawar.faiyaz.aziz@gmail.com>
2020-04-08 13:13:24 +0530

    commit

---
+++ lib/services/networking.dart
@@ -0,0 +1,15 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class NetworkHelper {
+  Future<dynamic> getData(String url) async {
+    try {
+      http.Response response = await http.get(url);
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+}
